3b12581c37e17aaf64b33bd099cb65e789d14135
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-08-14 16:16:33 -0300

    Dimension sizes

--- ext/nifticlib/nifticlib.c
+++ ext/nifticlib/nifticlib.c
@@ -98,6 +98,48 @@ static VALUE nifti_image_ndim(VALUE self){
   return INT2NUM(img->ndim);
 }
 
+static VALUE nifti_image_nx(VALUE self){
+  nifti_image *img = to_nifti_image(self);
+
+  return INT2NUM(img->nx);
+}
+
+static VALUE nifti_image_ny(VALUE self){
+  nifti_image *img = to_nifti_image(self);
+
+  return INT2NUM(img->ny);
+}
+
+static VALUE nifti_image_nz(VALUE self){
+  nifti_image *img = to_nifti_image(self);
+
+  return INT2NUM(img->nz);
+}
+
+static VALUE nifti_image_nt(VALUE self){
+  nifti_image *img = to_nifti_image(self);
+
+  return INT2NUM(img->nt);
+}
+
+static VALUE nifti_image_nu(VALUE self){
+  nifti_image *img = to_nifti_image(self);
+
+  return INT2NUM(img->nu);
+}
+
+static VALUE nifti_image_nv(VALUE self){
+  nifti_image *img = to_nifti_image(self);
+
+  return INT2NUM(img->nv);
+}
+
+static VALUE nifti_image_nw(VALUE self){
+  nifti_image *img = to_nifti_image(self);
+
+  return INT2NUM(img->nw);
+}
+
 /********/
 /* Init */
 /********/
@@ -114,6 +156,13 @@ void Init_nifticlib(void){
 
   // NIfTIImage methods
   rb_define_method(cNIfTIImage, "ndim", nifti_image_ndim, 0);
-  rb_define_method(cNIfTIImage, "data", nifti_image_data, 0);
+  rb_define_method(cNIfTIImage, "nx", nifti_image_nx, 0);
+  rb_define_method(cNIfTIImage, "ny", nifti_image_ny, 0);
+  rb_define_method(cNIfTIImage, "nz", nifti_image_nz, 0);
+  rb_define_method(cNIfTIImage, "nt", nifti_image_nt, 0);
+  rb_define_method(cNIfTIImage, "nu", nifti_image_nu, 0);
+  rb_define_method(cNIfTIImage, "nv", nifti_image_nv, 0);
+  rb_define_method(cNIfTIImage, "nw", nifti_image_nw, 0);
   rb_define_method(cNIfTIImage, "nvox", nifti_image_nvox, 0);
+  rb_define_method(cNIfTIImage, "data", nifti_image_data, 0);
 }
