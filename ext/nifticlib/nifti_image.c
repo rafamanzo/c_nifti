9f8584f97927e7b8a0ee3d23f8aac13016f6932f
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-08-20 20:06:17 -0300

    NIfTIImage transform methods initialization extracted

--- ext/nifticlib/nifti_image.c
+++ ext/nifticlib/nifti_image.c
@@ -65,32 +65,17 @@ VALUE nifti_image_data(VALUE self){
 VALUE init_nifti_image(VALUE module){
   VALUE cNIfTIImage = rb_define_class_under(module, "NIfTIImage", rb_cObject);
 
-  // Dimension methods
   cNIfTIImage = nifti_image_dimensions_init(cNIfTIImage);
-
-  // Spacing methods
   cNIfTIImage = nifti_image_spacings_init(cNIfTIImage);
-
   //Scaling methods
   rb_define_method(cNIfTIImage, "scl_slope", nifti_image_scl_slope, 0);
   rb_define_method(cNIfTIImage, "scl_inter", nifti_image_scl_inter, 0);
-
   //Calibration methods
   rb_define_method(cNIfTIImage, "cal_min", nifti_image_cal_min, 0);
   rb_define_method(cNIfTIImage, "cal_max", nifti_image_cal_max, 0);
-
-  // Timing methods
   cNIfTIImage = nifti_image_timings_init(cNIfTIImage);
-
-  // Quaternion methods
   cNIfTIImage = nifti_image_quaternions_init(cNIfTIImage);
-
-  // Transform methods
-  rb_define_method(cNIfTIImage, "qto_xyz", nifti_image_qto_xyz, 0);
-  rb_define_method(cNIfTIImage, "qto_ijk", nifti_image_qto_ijk, 0);
-  rb_define_method(cNIfTIImage, "sto_xyz", nifti_image_sto_xyz, 0);
-  rb_define_method(cNIfTIImage, "sto_ijk", nifti_image_sto_ijk, 0);
-
+  cNIfTIImage = nifti_image_transforms_init(cNIfTIImage);
   // Data methods
   rb_define_method(cNIfTIImage, "data", nifti_image_data, 0);
 
--- ext/nifticlib/nifti_image_transforms.c
+++ ext/nifticlib/nifti_image_transforms.c
@@ -27,3 +27,12 @@ VALUE nifti_image_sto_ijk(VALUE self){
 
   return mat44_to_rb_array(img->sto_ijk);
 }
+
+VALUE nifti_image_transforms_init(VALUE klass){
+  rb_define_method(klass, "qto_xyz", nifti_image_qto_xyz, 0);
+  rb_define_method(klass, "qto_ijk", nifti_image_qto_ijk, 0);
+  rb_define_method(klass, "sto_xyz", nifti_image_sto_xyz, 0);
+  rb_define_method(klass, "sto_ijk", nifti_image_sto_ijk, 0);
+
+  return klass;
+}
