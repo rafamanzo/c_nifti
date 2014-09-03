7b1861a96ca863888d84609a7170275a6fab8d61
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-09-02 22:53:07 -0300

    NIfTIImage constructor

--- ext/nifticlib/nifticlib.c
+++ ext/nifticlib/nifticlib.c
@@ -47,6 +47,34 @@ VALUE nifti_image_write_wrapper(VALUE self, VALUE rb_nifti_image, VALUE rb_outpu
   return rb_img;
 }
 
+/******************/
+/* New NIfTIImage */
+/******************/
+
+VALUE nifti_image_new(VALUE self, VALUE r_dims, VALUE r_datatype, VALUE r_data_fill){
+  int i = 0;
+  int *dims = NULL;
+  int datatype = 0;
+  int data_fill = 0;
+  nifti_image *img = NULL;
+
+  dims = malloc(RARRAY_LEN(r_dims)*sizeof(int));
+
+  for(i = 0; i < RARRAY_LEN(r_dims); i++){
+    dims[i] = NUM2INT(rb_ary_entry(r_dims, i));
+  }
+
+  datatype = NUM2INT(r_datatype);
+
+  data_fill = NUM2INT(r_data_fill);
+
+  img = nifti_make_new_nim(dims, datatype, data_fill);
+
+  free(dims);
+
+  return  Data_Wrap_Struct(cNIfTIImage, NULL, nifti_image_free, img);
+}
+
 /********/
 /* Init */
 /********/
@@ -60,4 +88,6 @@ void Init_nifticlib(void){
   rb_define_singleton_method(mNIfTICLib, "write", nifti_image_write_wrapper, 2);
 
   cNIfTIImage = init_nifti_image(mNIfTICLib);
+  // New
+  rb_define_singleton_method(cNIfTIImage, "new", nifti_image_new, 3);
 }
