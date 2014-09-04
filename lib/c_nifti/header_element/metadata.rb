4da693c92baedb73bf328502b5a78e029da60a38
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2014-09-04 19:40:54 -0300

    Finished metadata header part

--- lib/c_nifti/header_element/metadata.rb
+++ lib/c_nifti/header_element/metadata.rb
@@ -5,13 +5,45 @@ module CNifti
         @nifti_image.descrip
       end
 
+      def description=(value)
+        @nifti_image.descrip = value
+      end
+
       def aux_file
         @nifti_image.aux_file
+      end
+
+      def aux_file=(value)
+        @nifti_image.aux_file = value
       end
 
       def file_name
         @nifti_image.fname
       end
+
+      def file_name=(value)
+        @nifti_image.filenames = value # This actually sets iname and iname_offset as well
+      end
+
+      def image_name
+        @nifti_image.iname
+      end
+
+      def image_name_offset
+        @nifti_image.iname_offset
+      end
+
+      def swapsize
+        @nifti_image.swapsize
+      end
+
+      def byteorder
+        @nifti_image.byteorder
+      end
+
+      def analyze75_orient
+        @nifti_image.analyze75_orient
+      end      
     end
   end
 end
